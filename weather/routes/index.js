36678a24a658beaa93e02019de766a50cb5cc400
Kirchknopf <christian.kirchknopf@atos.net>
2020-07-08 22:05:47 +0200

    test

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 require('dotenv').config();
 const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
 const UNITS = process.env.UNITS || 'metric';
-const ENV = process.env;
+const ENV = JSON.stringify(process.env);
 const AUTHOR = process.env.AUTHOR;
 
 /* GET home page. */
