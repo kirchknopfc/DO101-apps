3a6d6a76be2902d7b5a01fadafaf9d0934696f37
Christian Kirchknopf <44832499+kirchknopfc@users.noreply.github.com>
2020-09-02 16:08:49 +0200

    Update index.js

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 });
 
 router.post('/get_weather', async function (req,res) {
+  let env = env
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
 
